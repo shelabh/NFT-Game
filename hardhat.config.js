b5358944ae6698fcf4e8c188a81d698e79cc1a94
Shelabh <68127067+shelabh@users.noreply.github.com>
2022-12-01 16:23:22 +0530

    created react app

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: 'YOUR_QUICKNODE_API_URL',
-      accounts: ['YOUR_PRIVATE_GOERLI_ACCOUNT_KEY'],
+      url: 'https://eth-goerli.g.alchemy.com/v2/9cAV3-tt89MOPHgf_aAyYDQWAeiMixJQ',
+      accounts: ['53243a2584ae3694d91e57a93caf707a8d47a62701f96d1a9be68ac7e763aff7'],
     },
   },
 };
