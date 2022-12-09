cd9c520a31dfaa013bad1144625b026c925cf09c
Shelabh <68127067+shelabh@users.noreply.github.com>
2022-12-10 03:06:15 +0530

    added static screen bg

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -4,10 +4,22 @@ import './index.css'
 
 const Home = () => {
 	return (
-		<div>
-			<Navbar />
-			<div className="home">
-				
+		<div className="view">
+			{/* <Navbar /> */}
+			
+			{/* <iframe title="aniamtion" src='https://my.spline.design/untitled-b50b7a3e9557621da407363141311eed/' frameborder='0' width='100%' height='100%'></iframe> */}
+			<div className='heading'>
+				Forefront
+			</div>
+			{/* <iframe title="animation" src='https://my.spline.design/noisedisplacecopy-79115317a729056443f145c436fbedac/' frameborder='0' width='100%' height='100%'></iframe> */}
+			
+			<div className="nav">
+				<button className='market'>
+					Marketplace
+				</button>
+				<button className='market'>
+					Sign In
+				</button>
 			</div>
 		</div>
 	)
