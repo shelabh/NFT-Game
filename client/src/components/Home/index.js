614b6214f27f4ba2dd394c90b10e96671c4fd7d1
Shelabh <68127067+shelabh@users.noreply.github.com>
2022-12-16 06:05:29 +0530

    added signin page

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,17 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import './index.css'
 
 const Home = () => {
+	// State
+	const [currentAccount, setCurrentAccount] = useState(null);
 
+	// Actions
+	const checkIfWalletIsConnected = async () => {
+		try {
+			const { ethereum } = window;
 	
+			if (!ethereum) {
+				console.log('Make sure you have MetaMask!');
+				return;
+			} else {
+				console.log('We have the ethereum object', ethereum);
+	
+				const accounts = await ethereum.request({ method: 'eth_accounts' });
+	
+				if (accounts.length !== 0) {
+					const account = accounts[0];
+					console.log('Found an authorized account:', account);
+					setCurrentAccount(account);
+					
+				} else {
+					console.log('No authorized account found');
+				}
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	/*
+   	* Implement your connectWallet method here
+   	*/
+	const connectWalletAction = async () => {
+		try {
+		  	const { ethereum } = window;
+	    
+		 	if (!ethereum) {
+		    		alert('Get MetaMask!');
+		    		return;
+		  	}
+	    
+			/*
+		   	* Fancy method to request access to account.
+		   	*/
+		  	const accounts = await ethereum.request({
+		    		method: 'eth_requestAccounts',
+		 	});
+	    
+		  	/*
+		  	 * Boom! This should print out public address once we authorize Metamask.
+		   	*/
+		  	console.log('Connected', accounts[0]);
+		  	setCurrentAccount(accounts[0]);
+			if (currentAccount === accounts) {
+				alert('You have authorized the account');
+			}
+		} catch (error) {
+		  	console.log(error);
+		}
+	};
+	    
+	useEffect(() => {
+		checkIfWalletIsConnected();
+	}, []);
+
 	return (
-		<div className="view">
+		<div className="page1">
 			{/* <Navbar /> */}
 			
 			{/* <iframe title="aniamtion" src='https://my.spline.design/untitled-b50b7a3e9557621da407363141311eed/' frameborder='0' width='100%' height='100%'></iframe> */}
 			<div className="connect">
-				<button className="btn-connect">
+				<button className="btn-connect" onClick={connectWalletAction}>
 					Connect Wallet
 				</button>
 			</div>
--- client/src/components/Signin/index.js
+++ client/src/components/Signin/index.js
@@ -1,11 +1,38 @@
-
+import './index.css'
 
 
 
 const Signin = () => {
 	return (
-		<div>
-			signin
+		<div className="page2">
+			<div className="left">
+				<div className="heading2">
+					Ah! Look Who's Back
+				</div>
+				<div className="inputs">
+					<input 
+						type="text" 
+						className="username" 
+						placeholder='username' 
+					/>
+					<input 
+						type="text" 
+						className="password" 
+						placeholder='password' 
+					/>
+				</div>
+				<div className="btns">
+					<button className="btn-signin">
+						Sign In
+					</button>
+					<button className="btn-signup">
+						Sign Up
+					</button>
+				</div>
+			</div>
+			<div className="right">
+				<img src="/images/signin.png" alt="Signin" draggale="false" />
+			</div>
 		</div>
 	)
 }
